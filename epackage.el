2cc77a3b9cfc4a1509bd6df6a25714ee220691c0
Jari Aalto <jari.aalto@cante.net>
2011-12-12 15:18:25 +0200

    (epackage--layout-template-info): Add field License-Text

--- epackage.el
+++ epackage.el
@@ -2111,6 +2111,7 @@ The files are written to %s. See function
 Package:
 Section: data | extensions | files | languages | mail | tools <M-x finder-list-keywords>
 License: GPL-2+ | BSD | Apache-2.0 | ... | Custom | None
+License-Text: <only for type Custom>
 Depends: emacs (>= 22)
 Status: core-emacs[-NN.N] unmaintained broken unsafe stable unstable experimental
 Compat:
